5e299cf6d8391d0e1e2e0be8a2fe4921874cab42
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-25 17:32:23 -0300

    Filling product page with SSG data

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,20 +1,39 @@
-import { useRouter } from "next/router"
-import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import { GetStaticPaths, GetStaticProps } from "next"
+import { stripe } from "../../lib/stripe"
+import Stripe from 'stripe'
 
-export default function Product() {
-    const { query } = useRouter()
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import Image from "next/future/image"
+import { useRouter } from "next/router"
+
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+    }
+}
+
+export default function Product({ product }: ProductProps) {
+    const { isFallback } = useRouter()
+
+    if (isFallback) {
+        return <p>Loading...</p>
+    }
 
     return (
         <ProductContainer>
             <ImageContainer>
-                {/* <Image /> */}
+                <Image src={product.imageUrl} width={520} height={480} alt=""/>
             </ImageContainer>
 
             <ProductDetails>
-                <h1>Camiseta X</h1>
-                <span>R$ 79,90</span>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
 
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet cupiditate similique saepe nobis illum dicta eum vero blanditiis autem id rem, quaerat, quasi corrupti quisquam iure pariatur commodi, voluptatum suscipit?</p>
+                <p>{product.description}</p>
 
                 <button>
                     Comprar
@@ -22,4 +41,38 @@ export default function Product() {
             </ProductDetails>
         </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params : { id: 'prod_MgAzSctrU7QsTe' } }
+        ],
+        fallback: true
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price']
+    })
+
+    const price = product.default_price as Stripe.Price
+    return {
+        props:{
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                }).format(price.unit_amount / 100),
+                description: product.description,
+            }
+        },
+        revalidate: 60 * 60 * 1 //1h
+    }
 }
