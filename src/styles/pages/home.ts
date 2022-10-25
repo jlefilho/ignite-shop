aca70a8078194d276753249b07ea522ccbb77f0c
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-25 11:42:39 -0300

    Created carousel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main',{
     display: 'flex',
-    gap: '3rem',
     width: '100%',
     maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
     marginLeft: 'auto',
@@ -12,7 +11,6 @@ export const HomeContainer = styled('main',{
 export const Product = styled('a', {
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     borderRadius: 8,
-    padding: '0.25rem',
     cursor: 'pointer',
     position: 'relative',
     overflow: 'hidden',
@@ -42,16 +40,17 @@ export const Product = styled('a', {
 
         transform: 'translateY(110%)',
         opacity: 0,
-        transition: 'all 0.2 ease-in-out',
+        transition: 'all 0.2s ease-in-out',
 
         strong: {
             fontSize: '$lg',
+            color: '$gray100'
         },
 
         span: {
             fontSize: '$xl',
             fontWeight: 'bold',
-            color: '$green30'
+            color: '$green300'
         }
     },
 
