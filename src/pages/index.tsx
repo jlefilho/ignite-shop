03eaab7b22cf01c12b2eee89808daf31d665a637
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-25 15:53:52 -0300

    Changed SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/future/image";
 
 import { useKeenSlider } from 'keen-slider/react'
@@ -6,10 +6,6 @@ import { stripe } from "../lib/stripe";
 import Stripe from 'stripe'
 
 import { HomeContainer, Product } from "../styles/pages/home";
-
-import shirt1 from '../assets/shirts/1.png'
-import shirt2 from '../assets/shirts/2.png'
-import shirt3 from '../assets/shirts/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 
@@ -49,7 +45,7 @@ export default function Home({ products }:HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -68,6 +64,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 //2h
   }
 }
