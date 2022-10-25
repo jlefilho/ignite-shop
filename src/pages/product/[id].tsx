32a9bd74d959099bc7b2200a3931edfc418e39e5
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-25 16:41:36 -0300

    Product page stylized

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,25 @@
 import { useRouter } from "next/router"
-
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                {/* <Image /> */}
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet cupiditate similique saepe nobis illum dicta eum vero blanditiis autem id rem, quaerat, quasi corrupti quisquam iure pariatur commodi, voluptatum suscipit?</p>
+
+                <button>
+                    Comprar
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
